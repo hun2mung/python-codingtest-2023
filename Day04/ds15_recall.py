da87f150046ab115c00257dea95d53a2954d50dc
hun2mung <ldh5444@naver.com>
2023-02-16 17:16:03 +0900

    Day14 23-02-16
    Day14

---
+++ Day04/ds15_recall.py
@@ -0,0 +1,7 @@
+def openBox():
+    print('종이상자를 엽니다.^^')
+    openBox()   # 자기자신을 다시 호출
+
+if __name__ == '__main__':
+    openBox()
+    
--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-a,b=map(int,input().split(' '))
-print(a/b)
+def openBox():
+    global count
+    print('종이 상자를 엽니다. ^^')
+    count -= 1
+    if count == 0:
+        print('** 반지를 넣고 반환합니다. **')
+        return  # 호출한 함수 위치로 복귀
+    print(count)
+    openBox()
+    print(count)
+    print('종이 상자를 닫습니다. ^^')
 
+count = 3
+openBox()
