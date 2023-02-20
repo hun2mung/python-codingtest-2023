71c767995e15a78c939c0b110e3e6227885d6ed9
hun2mung <ldh5444@naver.com>
2023-02-20 17:27:07 +0900

    Day16 (23-02-20)
    Day16

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
-def openBox():
-    global count
-    print('종이 상자를 엽니다. ^^')
-    count -= 1
-    if count == 0:
-        print('** 반지를 넣고 반환합니다. **')
-        return  # 호출한 함수 위치로 복귀
-    print(count)
-    openBox()
-    print(count)
-    print('종이 상자를 닫습니다. ^^')
-
-count = 3
-openBox()
+# 15552
+import sys
+input = sys.stdin.readline
+T = int(input())
+for _ in range(T):
+    a, b = map(int, input().split())
+    print(a+b)
