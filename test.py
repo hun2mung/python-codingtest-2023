6598b52126e543b4d2a4a889d63f229e24c41962
hun2mung <ldh5444@naver.com>
2023-02-24 16:15:09 +0900

    Day20(23-02-24)
    Day20

--- test.py
+++ test.py
@@ -1,14 +1,38 @@
-n = 1024
-answer = []
-def solution(n):
-    for i in range(2,n+1):
-        if n%i == 0:
-            result = n//i
-            n = result
-            answer.append(i)
-            return solution(n)
+n = 5
+lost = [4, 2]
+reserve = [3, 5]
 
+lost.sort()
+reserve.sort()
 
-if __name__ == '__main__':
-    solution(n)
-    print(list(set(answer)))
+reserve_set = [set(reserve) - set(lost)]
+lost_set = [set(lost) - set(reserve)]
+
+num = [0]*n
+
+for i in range(len(reserve_set)):
+    num[reserve_set[i]-1] = 100
+
+answer = 0
+cnt = 0
+for i in range(len(lost_set)):
+    for j in range(len(reserve_set)):
+        if ((int(lost_set[i])-1) or (int(lost_set[i])+1)) == int(reserve_set[j]):
+            k=lost_set[i]
+            reserve_set[j] = 100
+            num[k-1] = 100
+            lost_set[i] = 100
+            break
+        else:
+            continue
+
+for i in range(len(num)):
+    if num[i]==100:
+        cnt += 1
+
+print(cnt+(n-len(lost_set)-len(reserve_set)))
+
+# answer = cnt
+# if cnt > n:
+#     print(n)
+# print(answer)
